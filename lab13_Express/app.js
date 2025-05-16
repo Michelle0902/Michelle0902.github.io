8470a50974f1b138a8bf37c54231ad2353805395
Uyen Nguyen <uhuong571@gmail.com>
2025-05-15 23:03:54 -0500

    ex

--- lab13_Express/app.js
+++ lab13_Express/app.js
@@ -2,9 +2,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const calculatorRouter = require('./routes/calculator');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+// Serve static files from the "files" directory
+app.use('/files', express.static(path.join(__dirname, 'files')));
+
+// Routes to homepage
+app.get('/', (req, res) => {
+    res.send('Welcome to my website');
+});
+
+app.get('/home', (req, res) => {
+    res.send('Welcome to my website');
+});
+
+// Route to image
+app.get('/image', (req, res) => {
+    const imagePath = path.join(__dirname, 'files', 'sample.png');
+    res.sendFile(imagePath);
+});
+
+// Route to pdf
+app.get('/pdf', (req, res) => {
+    const pdfPath = path.join(__dirname, 'files', 'sample.pdf');
+    res.sendFile(pdfPath);
+});
+
+// Route to about text
+app.get('/about', (req, res) => {
+    const aboutPath = path.join(__dirname, 'files', 'about.txt');
+    res.sendFile(aboutPath);
+});
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +43,11 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/', calculatorRouter);
+
+// Handle 404 not found
+app.use((req, res) => {
+    res.status(404).send('Not Found');
+});
 
 // Start the server
 app.listen(PORT, () => {
